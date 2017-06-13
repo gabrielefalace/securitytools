3110e848a2146f2bd53b80c411c5c6152fba7544
Gabriele Falace <gabriele.falace@asideas.de>
2017-06-13 23:24:57 +0200

    finished functional programming

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -2,6 +2,11 @@ import com.rsk.Providers
 
 fun main(args: Array<String>) {
     getAllProviders{ key, value -> println("$key -> $value") }
+
+
+    val provider = Providers()
+    val allProviders = provider.getAllProviders("Random")
+    allProviders.forEach(::println)
 }
 
 private fun getAllProviders(fn: (String, String) -> Unit) {
--- src/main/kotlin/com/rsk/Providers.kt
+++ src/main/kotlin/com/rsk/Providers.kt
@@ -3,7 +3,34 @@ package com.rsk
 import java.security.Provider
 import java.security.Security
 
+data class ProviderDetails(val providerName: String, val name: String)
+
 class Providers {
+
+    fun getAllProvidersOld(filter: String): List<ProviderDetails> {
+        val providers = Security.getProviders()
+        val allProviders = mutableListOf<ProviderDetails>()
+
+        providers.forEach {
+            provider ->
+            val providerDetails = provider.entries
+                    .filter { it -> it.key.toString().contains(filter, true) }
+                    .map { ProviderDetails(provider.name, it.key.toString()) }
+            allProviders += providerDetails
+        }
+
+        return allProviders
+    }
+
+    fun getAllProviders(filter: String): List<ProviderDetails> {
+        val providers = Security.getProviders()
+
+        return providers.flatMap {
+            provider -> provider.entries
+                    .filter { it -> it.key.toString().contains(filter, true) }
+                    .map { ProviderDetails(provider.name, it.key.toString()) }
+        }
+    }
 
     companion object {
         fun getProviders(): List<Provider> {
